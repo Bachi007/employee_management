87627b7ff46407f3b4206609323e4b07334c10b7
DivyaRani01 <divya04200@gmail.com>
2022-10-21 22:33:12 +0530

    HrDao methods

--- src/main/java/com/empmng/config/hibernateUtil.java
+++ src/main/java/com/empmng/config/hibernateUtil.java
@@ -1,5 +1,62 @@
 package com.empmng.config;
 
-public class hibernateUtil {
+import java.util.Properties;
 
+import com.empmng.model.employee;
+import com.empmng.model.project;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+public class hibernateUtil {
+	
+	// creating session factory object
+		private static SessionFactory sesfac;
+
+		// getting logger in hibernateUtil class
+		static Logger log = Logger.getLogger(hibernateUtil.class);
+
+		public static SessionFactory getSessionFactory() {
+
+			// create a new session factory if not exist
+			if (sesfac == null) {
+
+				// setting properties of hibernate to connect with database
+				try {
+					Configuration config = new Configuration();
+					Properties pro = new Properties();
+
+					// setting hibernate properties to access database
+					pro.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver"); // getting mysql driver
+					pro.put(Environment.URL, "jdbc:mysql://localhost:3306/ProjectLive"); // connecting with database
+					pro.put(Environment.USER, "root"); // user name of Mysql server
+					pro.put(Environment.PASS, "Divya123"); // password of Mysql server
+					pro.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect"); // setting mysql dialect
+					pro.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread"); // checking the session by no of execution
+																					// of threads
+					pro.put(Environment.HBM2DDL_AUTO, "update"); // creating or updating the table if already exist
+					pro.put(Environment.SHOW_SQL, "false"); // by indicating false we are not going to show sql process in
+															// our console
+
+					config.setProperties(pro); // configure properties that we created
+					config.addAnnotatedClass(employee.class); // adding entity classes
+					config.addAnnotatedClass(project.class);
+					sesfac = config.buildSessionFactory();
+
+				} catch (Exception e) { // if any exception occur handling the exception
+					log.info(e.getMessage());
+				}
+			}
+			return sesfac;
+		}
+
+		// getSession method to create a new session from sessionFactory pool
+		// everytime this method is called
+		public static Session getSession() {
+
+			return getSessionFactory().openSession();
+		}
 }
--- src/main/java/com/empmng/daoimpl/hrdashboardImpl.java
+++ src/main/java/com/empmng/daoimpl/hrdashboardImpl.java
@@ -1,5 +1,116 @@
 package com.empmng.daoimpl;
 
-public class hrdashboardImpl {
+//importing required packages
+import java.util.List;
 
+import javax.persistence.Query;
+
+import com.empmng.config.hibernateUtil;
+import com.empmng.dao.hrdashboard;
+import com.empmng.exception.globalException;
+import com.empmng.model.employee;
+import com.empmng.model.project;
+import org.hibernate.Session;
+
+public class hrdashboardImpl implements hrdashboard {
+	
+//method 1 -> to print the list of all the employees present in the database
+	@Override
+	public List<employee> allEmployee() {
+		// TODO Auto-generated method stub
+		//Autoclosable session
+				try(Session ses=hibernateUtil.getSession()){
+					//getting rows of a employee table
+					String employee="employee";
+					Query qu=ses.createQuery("from employee where empDesignation=:employee").setParameter("employee", employee);
+					List<employee> employeeList=qu.getResultList();
+					return employeeList;
+				}
+	}
+	
+//method 2 -> to print the list of all the projects in the database
+	@Override
+	public List<project> allProjects() {
+		// TODO Auto-generated method stub
+		//autoclosable session object
+				try(Session ses=hibernateUtil.getSession()){
+					//getting rows of a project table
+					Query qu=ses.createQuery("from project");
+					List<project> projectList=qu.getResultList();
+					return projectList;
+				}
+
+	}
+	
+//method 3-> to view individual employee profile 
+	@Override
+	public employee viewEmployeeProfile(int empId) throws globalException  {
+		// TODO Auto-generated method stub
+		//Autoclosable session
+        try(Session ses=hibernateUtil.getSession()){
+			
+			employee e1=ses.get(employee.class, empId);
+			return e1;
+		}
+		
+	}
+	
+//method 4-> to delete the employee from the database
+	@Override
+	public int deltEmp(int empId) throws globalException {
+		// TODO Auto-generated method stub
+		//Autoclosable session
+				try(Session ses=hibernateUtil.getSession())
+				{
+					ses.beginTransaction();
+					int status= ses.createQuery("delete from employee where empId=:empId").setParameter("empId", empId).executeUpdate();
+					ses.getTransaction().commit();
+					return status;
+				}		
+	}
+	
+//method 5 -> to create a project in the database
+	@Override
+	public int createProject(project p) throws globalException {
+		// TODO Auto-generated method stub
+		//Autoclosable session
+				try(Session ses=hibernateUtil.getSession()){
+					
+					ses.beginTransaction();
+					String projectName= p.getProjectName;
+					project p2=null;
+					//checking for existing projectName
+					p2=(project)ses.createQuery("from project where projectName=:projectName").setParameter("projectName", projectName).uniqueResult();
+					//if projectName is unique then we can save the room
+					if(p2==null)
+					{
+						ses.save(p);
+						//commit
+						ses.getTransaction().commit();
+						return 1;
+					}
+					else {
+						throw new globalException(" Project  is already existed..!");
+					}
+					
+				}
+	}
+	
+//method 6 -> to allot a project to any employee 
+	@Override
+	public int allotProject(int empId, int pId) throws globalException{
+		// TODO Auto-generated method stub
+		//Autoclosable session
+				try(Session ses=hibernateUtil.getSession())
+				{
+					ses.beginTransaction();
+					int status=ses.createQuery("update employee set empProject_projectId=:rId where empId=:empId").setParameter("pId", pId).setParameter("empId", empId).executeUpdate();
+					ses.getTransaction().commit();
+					return status;
+					
+				}
+	}
+	
+
+	
 }
